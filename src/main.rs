430cf0912542e0eb9cb2e0d1f2f12c7320b86ce3
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2023-10-25 17:26:04 -0400

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,28 @@ async fn handle_connection(mut socket: TcpStream) {
         return;
     }
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let request = String::from_utf8_lossy(&buffer);
+
+    let response = match ingest_path(&request) {
+        "/" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n"
+    };
+
     if let Err(e) = socket.write_all(response.as_bytes()).await {
         println!("Failed to write response: {:?}", e);
     }
+}
+
+
+fn ingest_path(request: &str) -> &str {
+    if let Some(line) = request.lines().next() {
+        let chunks: Vec<&str> = line.split_whitespace().collect();
+
+        match chunks.as_slice() {
+            &["GET", path, "HTTP/1.1"] => return path,
+            _ => {}
+        }
+    }
+
+    "/"
 }
